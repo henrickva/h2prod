decb6afd1d6ad00b1273e81415271b932f32570e
Henrick Amaral <henrick.vaz.amaral@gmail.com>
2025-02-03 20:54:16 -0300

    mudando responsividade do portifólio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 import { X } from 'lucide-react';
+import test1 from '../../assets/teste3.png';
 import test2 from '../../assets/maryanaSite.png';
 import test3 from '../../assets/sevenSite.png';
 import test4 from '../../assets/mirandaSite.png'
 
 const projects = [
   {
-    title: 'Sites',
+    title1: 'Sites',
     category:'Mostrando ao publico pra que veio',
-    image: 'https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=2015&auto=format&fit=crop',
+    image: test1,
     description: 'Clique na imagem para ser redirecionado para o site',
     link:'https://dommeseventrix.vercel.app/',
     link2:'https://rainhamaryana.vercel.app/',
@@ -22,7 +23,10 @@ const projects = [
     title: 'Banners Redes Sociais',
     category:'Apresetando de forma rápida',
     image: test2,
-    description: 'Estratégia digital para lançamento de produto'
+    description: 'Estratégia digital para lançamento de produto',
+    image1: test3,
+    image2: test2,
+    image3: test4,
   },
   {
     title: 'Prévias de Video',
@@ -66,7 +70,7 @@ export default function Portfolio() {
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end p-6">
                   <div>
-                    <h3 className="text-xl font-semibold text-white mb-2">{project.title}</h3>
+                    <h3 className="text-xl font-semibold text-white mb-2">{project.title && project.title1 }</h3>
                     <p className="text-gray-300">{project.category}</p>
                     
                   </div>
@@ -79,7 +83,7 @@ export default function Portfolio() {
 
       {selectedProject && (
         <div className="fixed inset-0 bg-black/90 flex items-center justify-center z-50 p-4">
-          <div className="bg-zinc-800 rounded-lg max-w-md w-full h-[80vh] scrollbar flex flex-col relative p-6 overflow-y-auto">
+          <div className= {`bg-zinc-800 rounded-lg ${selectedProject.title1 ? 'max-w-xl':'w-11/12'}  h-[80vh] scrollbar flex flex-col relative p-6 overflow-y-auto`}>
             <button
               onClick={() => setSelectedProject(null)}
               className="absolute top-4 right-4 text-gray-400 hover:text-white"
@@ -88,7 +92,7 @@ export default function Portfolio() {
             </button>
             <h3 className="text-2xl font-bold text-white mb-2">{selectedProject.title}</h3>
             <p className='my-4'>{selectedProject.description ? selectedProject.description : selectedProject.title }</p>
-            <div className='grid grid-cols-1 gap-8'>
+            <div className={`grid gap-8 ${selectedProject.title1 ? 'grid-cols-1 ':'grid-cols-1 md:grid-cols-3'}`}>
               <a target='_blank' href={selectedProject.link ? selectedProject.link : selectedProject.image}>
                 <img
                   src={selectedProject.image1}
