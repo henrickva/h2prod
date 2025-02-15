107f4f2dfffac70a41971ac7635eb206bbc84b25
Henrick Amaral <henrick.vaz.amaral@gmail.com>
2025-02-14 22:36:50 -0300

    adicionando a funcionalidade do email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,38 @@
+import React, { useRef, useState } from 'react';
 import { Mail, Phone } from 'lucide-react';
-import { FaXTwitter } from "react-icons/fa6"
+import { FaXTwitter } from "react-icons/fa6";
 import { RiTelegram2Fill } from "react-icons/ri";
+import emailjs from '@emailjs/browser';
 
 export default function Contact() {
+  const form = useRef<HTMLFormElement>(null);
+  const [nameInput, setNameInput] = useState<string>("");
+  const [emailInput, setEmailInput] = useState<string>("");
+  const [messageInput, setMessageInput] = useState<string>("");
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (form.current) {
+      emailjs
+        .sendForm('service_s6wgo3b', 'template_ztarfaf', form.current, {
+          publicKey: '2aQ3Krv854d1PWPsD',
+        })
+        .then(
+          () => {
+            setNameInput("");
+            setEmailInput("");
+            setMessageInput("");
+            console.log('SUCCESSO!');
+            alert('Mensagem enviada com sucesso!!')
+          },
+          (error) => {
+            console.log('FALHA...', error.text);
+          },
+        );
+    }
+  };
+
   return (
     <div className="bg-zinc-900 py-20 px-4">
       <div className="max-w-6xl mx-auto">
@@ -15,16 +45,19 @@ export default function Contact() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
           <div>
-            <form className="space-y-6">
+            <form ref={form} onSubmit={sendEmail} className="space-y-6">
               <div>
                 <label htmlFor="name" className="block text-sm font-medium text-gray-300 mb-2">
                   Nome
                 </label>
                 <input
+                  value={nameInput}
                   type="text"
                   id="name"
+                  name="name"
                   className="w-full px-4 py-3 bg-zinc-800 border border-red-500 rounded-lg focus:outline-none text-white"
                   placeholder="Seu nome"
+                  onChange={(e) => setNameInput(e.target.value)}
                 />
               </div>
               <div>
@@ -32,10 +65,13 @@ export default function Contact() {
                   E-mail
                 </label>
                 <input
+                  value={emailInput}
                   type="email"
                   id="email"
+                  name="email"
                   className="w-full px-4 py-3 bg-zinc-800 border border-red-500 rounded-lg focus:outline-none text-white"
                   placeholder="seu@email.com"
+                  onChange={(e) => setEmailInput(e.target.value)}
                 />
               </div>
               <div>
@@ -43,13 +79,16 @@ export default function Contact() {
                   Mensagem
                 </label>
                 <textarea
+                  value={messageInput}
                   id="message"
+                  name="message"
                   rows={4}
                   className="w-full px-4 py-3 bg-zinc-800 border border-red-500 rounded-lg focus:outline-none text-white"
                   placeholder="Sua mensagem"
+                  onChange={(e) => setMessageInput(e.target.value)}
                 ></textarea>
               </div>
-              <button className="w-full bg-black ease-in-out duration-300 hover:bg-gradient-to-r from-red-400 to-orange-600 text-white font-semibold py-3 px-8 rounded-lg transition-all">
+              <button type="submit" className="w-full bg-black ease-in-out duration-300 hover:bg-gradient-to-r from-red-400 to-orange-600 text-white font-semibold py-3 px-8 rounded-lg transition-all">
                 Enviar mensagem
               </button>
             </form>
